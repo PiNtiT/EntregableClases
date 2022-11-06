2684c8a16282a4c1a4e14c3e781c365062d9b4b8
PinTiTA <85143340+PiNtiT@users.noreply.github.com>
2022-11-06 16:29:24 -0300

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio002
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; } 
+        public String Apellido { get; set; }
+        public String NombreUsuario { get; set; }
+        public String Contrasena { get; set; }
+        public String Mail { get; set; }
+
+    }
+}
