dff053325db8d305f39f5920a749a6569e856b54
PinTiTA <85143340+PiNtiT@users.noreply.github.com>
2022-11-14 19:58:29 -0300

    Update ProductoVendido.cs
    cambio clase internal por public

--- ProductoVendido.cs
+++ ProductoVendido.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ejercicio002
 {
-    internal class ProductoVendido
+    public class ProductoVendido
     {
         public int Id { get; set; }
         public int IdProducto { get; set; }
